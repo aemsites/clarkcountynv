ee1aedb3aa9d5897b398941f3503494b6650f0ed
Meet Kumar Jain <meejain@Meets-MacBook-Pro-2.local>
2024-11-02 01:56:55 +0530

    Adding an event listner for markers

--- blocks/accordion-ml/accordion-ml.js
+++ blocks/accordion-ml/accordion-ml.js
@@ -7,41 +7,48 @@
 function multilevelAccordion(mainUL) {
     const mainLIs = mainUL.children;
     for (let i = 0; i < mainLIs.length; i++) {
-      const mainLI = mainLIs[i];
-      console.log(mainLI);
-      const mainA = mainLI.querySelector('a');
+        const mainLI = mainLIs[i];
+        const mainA = mainLI.querySelector('a');
 
-      const details = document.createElement('details');
-      details.className = 'accordion-item';
+        const details = document.createElement('details');
+        details.className = 'accordion-item';
 
-      const summary = document.createElement('summary');
-      summary.className = 'accordion-item-label';
-      const labelLeft = document.createElement('p');
-      labelLeft.innerHTML = mainA.innerHTML;
-      const labelRight = document.createElement('div');
-      labelRight.classList.add('markerdiv');
-      const lablDiv = document.createElement('div');
-      lablDiv.append(labelLeft, labelRight);
-      summary.append(lablDiv);
+        const summary = document.createElement('summary');
+        summary.className = 'accordion-item-label';
+        const labelRight = document.createElement('div');
+        labelRight.classList.add('markerdiv');
+        const lablDiv = document.createElement('div');
+        lablDiv.append(mainA, labelRight);
+        summary.append(lablDiv);
 
-      const childUL = mainLI.querySelector('ul');
+        const childUL = mainLI.querySelector('ul');
 
-      if (childUL) {
-        details.append(summary, childUL);
-        mainLI.replaceWith(details);
-        multilevelAccordion(childUL);
-      } else {
-        details.append(summary);
-        mainLI.replaceWith(details)
-      }
+        if (childUL) {
+            details.append(summary, childUL);
+            mainLI.replaceWith(details);
+            multilevelAccordion(childUL);
+        } else {
+            details.append(summary);
+            mainLI.replaceWith(details)
+        }
     }
-  }
+    mainUL.querySelectorAll('details').forEach((details) => {
+        details.addEventListener('toggle', (event) => {
+            if (event.target.open) {
+                console.log(event.target);
+                details.parentElement.querySelectorAll('details').forEach((details) => {
+                    if (details !== event.target) {
+                        details.removeAttribute('open');
+                    }
+                });
+            }
+        });
+    });
+}
 
 
 export default function decorate(block) {
-    console.log(block);
     const mainUL = document.querySelector('ul');
-    console.log(mainUL);
     multilevelAccordion(mainUL);
     [...block.children].forEach((row) => {
         console.log(row);
