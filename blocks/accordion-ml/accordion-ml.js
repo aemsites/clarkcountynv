1684a7b971d30e2135e8ed6b7adc391113675974
Meet Kumar Jain <meejain@Meets-MacBook-Pro-2.local>
2024-11-01 15:55:38 +0530

    Intstrumenting the multi level header

---
+++ blocks/accordion-ml/accordion-ml.js
@@ -0,0 +1,43 @@
+/*
+ * Accordion Block
+ * Recreate an accordion
+ * https://www.hlx.live/developer/block-collection/accordion
+ */
+
+function multilevelAccordion(mainUL) {
+    const mainLIs = mainUL.children;
+    for (let i = 0; i < mainLIs.length; i++) {
+      const mainLI = mainLIs[i];
+      console.log(mainLI);
+      const mainA = mainLI.querySelector('a');
+
+      const details = document.createElement('details');
+      details.className = 'accordion-item';
+
+      const summary = document.createElement('summary');
+      summary.className = 'accordion-item-label';
+      summary.append(mainA);
+
+      const childUL = mainLI.querySelector('ul');
+
+      if (childUL) {
+        details.append(summary, childUL);
+        mainLI.replaceWith(details);
+        multilevelAccordion(childUL);
+      } else {
+        details.append(summary);
+        mainLI.replaceWith(details)
+      }
+    }
+  }
+
+
+export default function decorate(block) {
+    console.log(block);
+    const mainUL = document.querySelector('ul');
+    console.log(mainUL);
+    multilevelAccordion(mainUL);
+    [...block.children].forEach((row) => {
+        console.log(row);
+    });
+  }
