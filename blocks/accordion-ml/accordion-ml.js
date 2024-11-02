91e6a9a8d5ff42f2c01ca5c286e35bcde460e449
Meet Kumar Jain <meejain@p01kbdb03.macromedia.com>
2024-11-02 17:57:48 +0530

    improving the CSS further

--- blocks/accordion-ml/accordion-ml.js
+++ blocks/accordion-ml/accordion-ml.js
@@ -4,17 +4,19 @@
  * https://www.hlx.live/developer/block-collection/accordion
  */
 
-function multilevelAccordion(mainUL, itemClass, labelClass, level) {
+function multilevelAccordion(mainUL, itemClass, labelClass, level, itemcolor) {
+    let counter = 0;
     const mainLIs = mainUL.children;
     for (let i = 0; i < mainLIs.length; i++) {
         const mainLI = mainLIs[i];
         const mainA = mainLI.querySelector('a');
 
         const details = document.createElement('details');
-        details.className = `${itemClass}`;
+        details.classList.add(`${itemClass}`);
 
         const summary = document.createElement('summary');
-        summary.className = `${labelClass}`;
+        summary.classList.add(`${labelClass}`);
+        summary.classList.add(`${itemcolor}`);
         const labelRight = document.createElement('div');
         labelRight.classList.add('markerdiv');
         const lablDiv = document.createElement('div');
@@ -24,21 +26,48 @@ function multilevelAccordion(mainUL, itemClass, labelClass, level) {
         const childUL = mainLI.querySelector('ul');
 
         if (childUL) {
-            level++;
             details.append(summary, childUL);
             mainLI.replaceWith(details);
             const newitemClass = `accordion-item-level${level}`;
             const newlabelClass = `accordion-item-label-level${level}`;
-            multilevelAccordion(childUL, newitemClass, newlabelClass, level);
+            const newitemcolor = `itemcolor${level}`;   
+            multilevelAccordion(childUL, newitemClass, newlabelClass, level, newitemcolor);
         } else {
+            console.log(level);
             details.append(summary);
             mainLI.replaceWith(details)
         }
     }
+}
+
+function findLevel(element) {
+    let level = 0;
+    while (element.parentElement) {
+        if (element.parentElement.tagName === 'UL') {
+            level++;
+        }
+        element = element.parentElement;
+    }
+    return level;
+}
+
+export default function decorate(block) {
+    const mainUL = document.querySelector('ul');
+    const level = 1;
+    multilevelAccordion(mainUL, 'accordion-item-level1', 'accordion-item-label-level1', level, 'itemcolor1');
+    [...block.children].forEach((row) => {
+        console.log(row);
+    });
+
+    console.log(mainUL);
     mainUL.querySelectorAll('details').forEach((details) => {
         details.addEventListener('toggle', (event) => {
             if (event.target.open) {
-                console.log(event.target);
+                const value = findLevel(event.target);
+                console.log(value);
+                event.target.querySelector('ul').querySelectorAll(':scope > details').forEach((details) => {
+                    details.querySelector('summary').classList.add('itemcolor'+(value+1)); 
+                });
                 details.parentElement.querySelectorAll('details').forEach((details) => {
                     if (details !== event.target) {
                         details.removeAttribute('open');
@@ -47,14 +76,6 @@ function multilevelAccordion(mainUL, itemClass, labelClass, level) {
             }
         });
     });
-}
 
-
-export default function decorate(block) {
-    const mainUL = document.querySelector('ul');
-    const level = 1;
-    multilevelAccordion(mainUL, 'accordion-item-level1', 'accordion-item-label-level1', level);
-    [...block.children].forEach((row) => {
-        console.log(row);
-    });
+    console.log(block.querySelector('details').getAttribute('open', ''));
   }
