f4920b10a6024266efcd8a9add32cd82331e87a9
Meet Kumar Jain <meejain@Meets-MacBook-Pro-2.local>
2024-11-02 12:52:42 +0530

    improving the CSS further

--- blocks/accordion-ml/accordion-ml.js
+++ blocks/accordion-ml/accordion-ml.js
@@ -4,17 +4,17 @@
  * https://www.hlx.live/developer/block-collection/accordion
  */
 
-function multilevelAccordion(mainUL) {
+function multilevelAccordion(mainUL, itemClass, labelClass, level) {
     const mainLIs = mainUL.children;
     for (let i = 0; i < mainLIs.length; i++) {
         const mainLI = mainLIs[i];
         const mainA = mainLI.querySelector('a');
 
         const details = document.createElement('details');
-        details.className = 'accordion-item';
+        details.className = `${itemClass}`;
 
         const summary = document.createElement('summary');
-        summary.className = 'accordion-item-label';
+        summary.className = `${labelClass}`;
         const labelRight = document.createElement('div');
         labelRight.classList.add('markerdiv');
         const lablDiv = document.createElement('div');
@@ -24,9 +24,12 @@ function multilevelAccordion(mainUL) {
         const childUL = mainLI.querySelector('ul');
 
         if (childUL) {
+            level++;
             details.append(summary, childUL);
             mainLI.replaceWith(details);
-            multilevelAccordion(childUL);
+            const newitemClass = `accordion-item-level${level}`;
+            const newlabelClass = `accordion-item-label-level${level}`;
+            multilevelAccordion(childUL, newitemClass, newlabelClass, level);
         } else {
             details.append(summary);
             mainLI.replaceWith(details)
@@ -49,7 +52,8 @@ function multilevelAccordion(mainUL) {
 
 export default function decorate(block) {
     const mainUL = document.querySelector('ul');
-    multilevelAccordion(mainUL);
+    const level = 1;
+    multilevelAccordion(mainUL, 'accordion-item-level1', 'accordion-item-label-level1', level);
     [...block.children].forEach((row) => {
         console.log(row);
     });
