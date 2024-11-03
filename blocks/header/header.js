f6b06f9d8157f5d26012f1483637564ce16f7437
Meet Kumar Jain <meejain@p01kbdb03.macromedia.com>
2024-11-04 02:36:26 +0530

    Adding the JS logic for mobile

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -116,8 +116,50 @@ function toggleMenu(nav, navSections, forceExpanded = null) {
   }
 }
 
+function decorateNavItemMobile(mainUL) {
+  const mainLIs = mainUL.children;
+  for (let i = 0; i < mainLIs.length; i += 1) {
+    const mainLI = mainLIs[i];
+    const mainA = mainLI.querySelector('a');
+
+    const details = document.createElement('details');
+    details.classList.add('accordion-item');
+
+    const summary = document.createElement('summary');
+    summary.classList.add('accordion-item-label');
+    const labelRight = document.createElement('div');
+    labelRight.classList.add('markerdiv');
+    const lablDiv = document.createElement('div');
+    lablDiv.append(mainA, labelRight);
+    summary.append(lablDiv);
+
+    const childUL = mainLI.querySelector('ul');
+
+    if (childUL) {
+      details.append(summary, childUL);
+      mainLI.replaceWith(details);
+      decorateNavItemMobile(childUL);
+    } else {
+      details.append(summary);
+      mainLI.replaceWith(details);
+    }
+  }
+}
+
+function findLevel(element) {
+  let ele = element;
+  let level = 0;
+  while (ele.parentElement) {
+    if (ele.parentElement.tagName === 'UL') {
+      level += 1;
+    }
+    ele = ele.parentElement;
+  }
+  return level;
+}
+
 function decorateNavItem(parent, navSectionSearchItem) {
-  const menuUl = div({ class: 'menuUL' });
+  const menuUl = div({ class: 'menuul' });
   const navIn = div({ class: 'nav-in' });
   const navContent = div({ class: 'nav-content' });
   const navContentIn = div({ class: 'nav-content-in' });
@@ -226,23 +268,56 @@ export default async function decorate(block) {
   const navSections = nav.querySelector('.nav-sections');
   if (navSections) {
     const navSectionSearchItem = navSections.children[0]?.children[1];
-    navSections.querySelectorAll(':scope .default-content-wrapper > ul > li').forEach((navSection) => {
-      if (navSection.querySelector('ul')) navSection.classList.add('nav-drop');
-      console.log(navSection);
-      decorateNavItem(navSection, navSectionSearchItem);
-      navSection.addEventListener('mouseover', () => {
-        if (isDesktop.matches) {
-          toggleAllNavSections(navSections);
-          navSection.setAttribute('aria-expanded', 'true');
-        }
+    if (isDesktop.matches) {
+      navSections.querySelectorAll(':scope .default-content-wrapper > ul > li').forEach((navSection) => {
+        if (navSection.querySelector('ul')) navSection.classList.add('nav-drop');
+        decorateNavItem(navSection, navSectionSearchItem);
+        navSection.addEventListener('mouseover', () => {
+          if (isDesktop.matches) {
+            toggleAllNavSections(navSections);
+            navSection.setAttribute('aria-expanded', 'true');
+          }
+        });
+        navSection.addEventListener('mouseout', () => {
+          if (isDesktop.matches) {
+            toggleAllNavSections(navSections);
+            navSection.setAttribute('aria-expanded', 'false');
+          }
+        });
       });
-      navSection.addEventListener('mouseout', () => {
-        if (isDesktop.matches) {
-          toggleAllNavSections(navSections);
-          navSection.setAttribute('aria-expanded', 'false');
-        }
+    } else {
+      const mainUL = navSections.querySelector(':scope .default-content-wrapper > ul');
+      decorateNavItemMobile(mainUL);
+      mainUL.querySelectorAll('details').forEach((details) => {
+        details.addEventListener('toggle', (event) => {
+          if (event.target.open) {
+            const value = findLevel(event.target);
+            event.target.querySelector('ul').querySelectorAll(':scope > details').forEach((ele) => {
+              ele.querySelector('summary').classList.add(`itemcolor${value + 1}`);
+            });
+            details.parentElement.querySelectorAll('details').forEach((ele) => {
+              if (ele !== event.target) {
+                ele.removeAttribute('open');
+              }
+            });
+          }
+        });
       });
-    });
+      mainUL.querySelectorAll(':scope > li').forEach((navSection) => {
+        navSection.addEventListener('mouseover', () => {
+          if (isDesktop.matches) {
+            toggleAllNavSections(navSections);
+            navSection.setAttribute('aria-expanded', 'true');
+          }
+        });
+        navSection.addEventListener('mouseout', () => {
+          if (isDesktop.matches) {
+            toggleAllNavSections(navSections);
+            navSection.setAttribute('aria-expanded', 'false');
+          }
+        });
+      });
+    }
     navSectionSearchItem.remove();
   }
 
