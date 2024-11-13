1a020c68d17b2cc30e3db7b86a1f6ad856643593
Meet Kumar Jain <meejain@p01kbdb03.macromedia.com>
2024-11-14 02:40:32 +0530

    Removing the extra code & authenticating with user id

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -6,42 +6,13 @@ sampleRUM('cwv');
 
 const isDesktop = window.matchMedia('(min-width: 900px)');
 
-function applyMaxWidthToIframe(counter) {
-  const iframe = document.querySelector('iframe');
-  if (iframe) {
-    iframe.style.setProperty('max-width', '350px', 'important');
-    iframe.contentDocument.body.style.setProperty('--mainColorRgbValues', '#0094c3', 'important');
-    iframe.addEventListener('load', () => {
-      console.log(iframe);
-    });
-  } else if (counter < 100) {
-    setTimeout(() => applyMaxWidthToIframe(counter + 1), 200);
-  }
-}
-
-function applyBackground(counter) {
-    const div = document.querySelector('.userway_buttons_wrapper .uai.userway_dark');
-    if (div) {
-      div.style.setProperty('background', 'rgb(0, 148, 195)', 'important');
-      const widget = document.querySelector('body > div.uwy.userway_p1.utb > div');
-      widget.style.opacity = '1';
-    } else if (counter < 5000) {
-      setTimeout(() => applyBackground(counter + 1), 5);
-    }
-  }
-
 async function loadWidget() {
-  await loadCSS('/widgets/accessibility/accessibility.css');
-  await loadScript('/widgets/accessibility/accessibility.js').then(() => {
-    applyBackground(0);
-    applyMaxWidthToIframe(0);
-  });
+  await loadScript('/widgets/accessibility/accessibility.js');
 }
 
 if (isDesktop.matches) {
   loadWidget();
 }
-
 
 function resizeAction() {
     if (!isDesktop.matches) {
