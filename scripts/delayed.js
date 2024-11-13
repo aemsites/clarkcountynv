8cfb75580d806c7915faa8995f03c52152008833
Meet Kumar Jain <meejain@p01kbdb03.macromedia.com>
2024-11-14 01:12:14 +0530

    Adding the logic for screen resize for widget. In mobile - display = off

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -4,27 +4,26 @@ import { loadScript, loadCSS, sampleRUM } from './aem.js';
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 
+const isDesktop = window.matchMedia('(min-width: 900px)');
+
 function applyMaxWidthToIframe(counter) {
-    const iframe = document.querySelector('iframe');
-    if (iframe) {
-        // iframe.addEventListener('load', () => {
-        //     console.log(iframe);
-        //     iframe.style.setProperty('max-width', '350px', 'important');
-        //     iframe.contentDocument.body.style.setProperty('--mainColorRgbValues', '#0094c3', 'important');
-        // });
-      iframe.style.setProperty('max-width', '350px', 'important');
-      iframe.contentDocument.body.style.setProperty('--mainColorRgbValues', '#0094c3', 'important');
-    } else if (counter < 100) {
-      setTimeout(() => applyMaxWidthToIframe(counter + 1), 200);
-    }
+  const iframe = document.querySelector('iframe');
+  if (iframe) {
+    iframe.style.setProperty('max-width', '350px', 'important');
+    iframe.contentDocument.body.style.setProperty('--mainColorRgbValues', '#0094c3', 'important');
+    iframe.addEventListener('load', () => {
+      console.log(iframe);
+    });
+  } else if (counter < 100) {
+    setTimeout(() => applyMaxWidthToIframe(counter + 1), 200);
   }
+}
 
 function applyBackground(counter) {
     const div = document.querySelector('.userway_buttons_wrapper .uai.userway_dark');
     if (div) {
       div.style.setProperty('background', 'rgb(0, 148, 195)', 'important');
       const widget = document.querySelector('body > div.uwy.userway_p1.utb > div');
-    //   document.documentElement.style.setProperty('--mainColorRgbValues', '#0094c3', 'important');
       widget.style.opacity = '1';
     } else if (counter < 5000) {
       setTimeout(() => applyBackground(counter + 1), 5);
@@ -32,11 +31,26 @@ function applyBackground(counter) {
   }
 
 async function loadWidget() {
-    await loadCSS('/widgets/accessibility/accessibility.css');
-    await loadScript('/widgets/accessibility/accessibility.js').then(() => {
-      applyBackground(0);
-      applyMaxWidthToIframe(0);
-    });
-  }
+  await loadCSS('/widgets/accessibility/accessibility.css');
+  await loadScript('/widgets/accessibility/accessibility.js').then(() => {
+    applyBackground(0);
+    applyMaxWidthToIframe(0);
+  });
+}
 
-loadWidget();
+if (isDesktop.matches) {
+  loadWidget();
+}
+
+
+function resizeAction() {
+    if (!isDesktop.matches) {
+      const div = document.querySelector('.userway_buttons_wrapper .uai.userway_dark');
+      div.style.setProperty('display', 'none');
+    } else {
+      const div = document.querySelector('.userway_buttons_wrapper .uai.userway_dark');
+      div.style.setProperty('display', 'block');
+    }
+}
+
+window.addEventListener('resize', resizeAction);
