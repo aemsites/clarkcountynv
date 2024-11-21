4f771d2fc2ab82b9a0c4c8c64f5bea9acbe95735
Meet Kumar Jain <meejain@p01kbdb03.macromedia.com>
2024-11-22 04:51:49 +0530

    Implementing the default template

--- templates/default/default.js
+++ templates/default/default.js
@@ -1,22 +1,14 @@
 // eslint-disable-next-line no-unused-vars,no-empty-function
 import {
-  section, div,
+  div,
 } from '../../scripts/dom-helpers.js';
 
 export default async function decorate(doc) {
   const $main = doc.querySelector('main');
-  const $mainChildren = Array.from($main.childNodes);
-  const $section = section();
   const $leftsection = document.querySelector('.leftsection');
   const $rightsection = document.querySelector('.rightsection');
-  const $mainmenu = section({ class: 'mainmenu' });
+  const $mainmenu = div({ class: 'mainmenu' });
 
-  $mainChildren.forEach((child) => {
-    $section.appendChild(child);
-  });
-
-  $main.innerHTML = '';
   $mainmenu.append($leftsection, $rightsection);
-  console.log($mainmenu);
-  $main.append($section, $mainmenu);
+  $main.append($mainmenu);
 }
