6a0db254e34cdf675548edc48a507c3458f7c71f
Meet Kumar Jain <meejain@p01kbdb03.macromedia.com>
2024-11-22 03:21:32 +0530

    Implementing Mobile view of multilevel dropdown

---
+++ blocks/accordion-ml/accordion-ml.js
@@ -0,0 +1,62 @@
+function multilevelAccordion(mainUL) {
+  const mainLIs = mainUL.children;
+  for (let i = 0; i < mainLIs.length; i += 1) {
+    const mainLI = mainLIs[i];
+    const mainA = mainLI.querySelector('a');
+
+    const details = document.createElement('details');
+    details.classList.add('accordion-item');
+
+    const summary = document.createElement('summary');
+    summary.classList.add('accordion-item-label');
+    const labelRight = document.createElement('div');
+    labelRight.classList.add('markerdiv');
+    const lablDiv = document.createElement('div');
+    lablDiv.append(mainA, labelRight);
+    summary.append(lablDiv);
+
+    const childUL = mainLI.querySelector('ul');
+
+    if (childUL) {
+      details.append(summary, childUL);
+      mainLI.replaceWith(details);
+      multilevelAccordion(childUL);
+    } else {
+      details.append(summary);
+      mainLI.replaceWith(details);
+    }
+  }
+}
+
+function findLevel(element) {
+  let ele = element;
+  let level = 0;
+  while (ele.parentElement) {
+    if (ele.parentElement.tagName === 'UL') {
+      level += 1;
+    }
+    ele = ele.parentElement;
+  }
+  return level;
+}
+
+export default function decorate(block) {
+  console.log(block);
+  const mainUL = document.querySelector('ul');
+  multilevelAccordion(mainUL);
+  mainUL.querySelectorAll('details').forEach((details) => {
+    details.addEventListener('toggle', (event) => {
+      if (event.target.open) {
+        const value = findLevel(event.target);
+        event.target.querySelector('ul').querySelectorAll(':scope > details').forEach((ele) => {
+          ele.querySelector('summary').classList.add(`itemcolor${value + 1}`);
+        });
+        details.parentElement.querySelectorAll('details').forEach((ele) => {
+          if (ele !== event.target) {
+            ele.removeAttribute('open');
+          }
+        });
+      }
+    });
+  });
+}
