b866f2f9324ac6ce47c3aa3ae2674c92c23340da
Kent Livingston <klivings@Kents-MacBook-Pro.local>
2024-11-21 16:50:59 -0600

    fixes for icon

--- widgets/share-button/share-button.js
+++ widgets/share-button/share-button.js
@@ -1,14 +1,12 @@
 function widget() {
-  // class default-content-wrapper
-  // const btn = document.createElement('div');
+  // widget for share modal launch
   const a = document.createElement('a');
   const img = document.createElement('div');
   const txt = document.createElement('span');
 
-  img.innerHTML = '<img data-icon-name="share" src="/icons/share.svg" alt="" loading="lazy">';
+  img.innerHTML = '<img data-icon-name="share" src="/icons/icons8-share-18.svg" alt="" loading="lazy">';
   a.setAttribute('href', '/modals/share-with-social-network');
   a.setAttribute('class', 'floating-share-btn');
-  // img.setAttribute('class', 'share-float');
   a.append(img);
   txt.innerHTML = 'Share';
   a.append(txt);
