a491dd4243115addcf63fd291b12f6a24a34350f
Meet Kumar Jain <meejain@p01kbdb03.macromedia.com>
2024-12-05 19:14:20 +0530

    Updating a variant clickable for cards

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -4,13 +4,31 @@ export default function decorate(block) {
   /* change to ul, li */
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
-    const li = document.createElement('li');
-    while (row.firstElementChild) li.append(row.firstElementChild);
-    [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
-    });
-    ul.append(li);
+    if (block.classList.contains('clickable')) {
+      const li = document.createElement('li');
+      const aEle = document.createElement('a');
+      aEle.append(li);
+      while (row.firstElementChild) li.append(row.firstElementChild);
+      [...li.children].forEach((div) => {
+        if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
+        else {
+          div.className = 'cards-card-body';
+          if (div.querySelector('a')) {
+            aEle.href = div.querySelector('a').href;
+            div.querySelector('a').remove();
+          }
+        }
+      });
+      ul.append(aEle);
+    } else {
+      const li = document.createElement('li');
+      while (row.firstElementChild) li.append(row.firstElementChild);
+      [...li.children].forEach((div) => {
+        if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
+        else div.className = 'cards-card-body';
+      });
+      ul.append(li);
+    }
   });
   ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
   block.textContent = '';
