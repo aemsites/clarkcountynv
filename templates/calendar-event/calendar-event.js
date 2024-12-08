40c84764f9e3c0996f0ca73ce05afac8c565972c
Meet Kumar Jain <meejain@Meets-MacBook-Pro-2.local>
2024-12-08 22:56:11 +0530

    Adding the logic for Hero element for event template

--- templates/calendar-event/calendar-event.js
+++ templates/calendar-event/calendar-event.js
@@ -1,7 +1,17 @@
 // eslint-disable-next-line no-unused-vars,no-empty-function
 
+import { getMetadata } from '../../scripts/aem.js';
+
 export default async function decorate(doc) {
   doc.querySelector('header').remove();
   doc.querySelector('footer').remove();
   doc.body.classList.add('calendar-event');
+
+  // Change the background color of the hero date & time based on the division-color metadata
+  const divisionColor = getMetadata('division-color');
+  if (divisionColor) {
+    doc.querySelectorAll('body.calendar-event main .section.hero .hero-wrapper .block.hero div div').forEach((element) => {
+      element.style.backgroundColor = divisionColor;
+    });
+  }
 }
