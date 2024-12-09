422852dc79d85e7ab5f05f12ba3e2f96546b8bdc
Meet Kumar Jain <meejain@Meets-MacBook-Pro-2.local>
2024-12-10 02:09:47 +0530

    Adding the logic of recurring

--- templates/calendar/calendar.js
+++ templates/calendar/calendar.js
@@ -1,12 +1,20 @@
 import { div, section } from '../../scripts/dom-helpers.js';
 
-// const childCalendars = [
-//   'County Commissioners',
-//   'County Commission District A',
-//   'County Commission District B',
-//   'County Commissioners District C',
-//   'County Commissioners District D',
-// ];
+class Obj {
+  constructor(title, start, end, allDay, daysOfWeek, startTime, endTime, startRecur, endRecur, url) {
+    this.title = title;
+    this.start = start;
+    this.end = end;
+    this.allDay = allDay;
+    this.daysOfWeek = daysOfWeek;
+    this.startTime = startTime;
+    this.endTime = endTime;
+    this.startRecur = startRecur;
+    this.endRecur = endRecur;
+    this.url = url;
+  }
+}
+
 
 // Fetching events from individual calendar sheets
 export async function fetchPlaceholders(prefix) {
@@ -39,28 +47,73 @@ export async function fetchPlaceholders(prefix) {
 }
 
 async function initializeCalendar() {
-  let eventsData = [];
+  let importedData = [];
+  let eventsList = [];
   const calendarEl = document.getElementById('calendar');
   // const data = getEventsManual();
   const normalizeCalendar = 'events';
   const placeholders = await fetchPlaceholders(normalizeCalendar);
-  eventsData = [...eventsData, ...placeholders.data];
+  importedData = [...importedData, ...placeholders.data];
+  console.log(importedData);
   // eslint-disable-next-line no-undef
   const calendar = new FullCalendar.Calendar(calendarEl, {
     initialView: 'dayGridMonth',
     headerToolbar: {
       left: 'prev,next today',
-      center: 'title',
+      center: 'addEventButton',
       right: 'dayGridMonth,timeGridWeek,timeGridDay',
     },
     navLinks: true, // can click day/week names to navigate views
     editable: true,
     selectable: true,
     dayMaxEvents: true,
-    events: eventsData,
+    // events: importedData,
     eventTimeFormat: { hour: 'numeric', minute: '2-digit' },
   });
   calendar.render();
+  importedData.forEach((event) => {
+    console.log(event.startRecur);
+    const startTime = event.startRecur.split('T')[1];
+    const endTime = event.endRecur.split('T')[1];
+    const url = window.location.origin + event.path;
+    const eventObj = new Obj(event.title, event.start, event.end, event.allDay, event.daysOfWeek, startTime, endTime, event.startRecur, event.endRecur, url);
+    eventsList.push(eventObj);
+  });
+  console.log(eventsList);
+  eventsList.forEach((event) => {
+    if (event.daysOfWeek.length > 0) {
+      console.log(event);
+      calendar.addEvent({
+        title: event.title,
+        allDay: false,
+        daysOfWeek: event.daysOfWeek,
+        startTime: event.startTime,
+        endTime: event.endTime,
+        startRecur: event.startRecur,
+        endRecur: event.endRecur,
+        url: event.url,
+      });
+    }
+    else {
+      calendar.addEvent({
+        title: event.title,
+        start: event.start,
+        end: event.end,
+        allDay: event.allDay,
+        url: event.url,
+      });
+    }
+  });
+  // calendar.addEvent({
+  //   // start: date,
+  //   allDay: false,
+  //   title: 'Testing Recurring Event',
+  //   daysOfWeek: [2,5],
+  //   startTime: '13:00',
+  //   endTime: '14:00',
+  //   startRecur: '2024-12-25',
+  //   endRecur: '2025-01-25',
+  // });
 }
 
 export function loadfullcalendar() {
