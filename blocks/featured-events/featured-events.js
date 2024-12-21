cb4df241a468ed0726a6e17073998b8771608bbc
Meet Kumar Jain <meejain@Meets-MacBook-Pro-2.local>
2024-12-22 02:06:23 +0530

    Adding placeholder for events

--- blocks/featured-events/featured-events.js
+++ blocks/featured-events/featured-events.js
@@ -1,3 +1,38 @@
+import { div, h3 } from '../../scripts/dom-helpers.js';
+import {
+  createOptimizedPicture, buildBlock, decorateBlock, loadBlock,
+} from '../../scripts/aem.js';
+
+// Result parsers parse the query results into a format that can be used by the block builder for
+// the specific block types
+const resultParsers = {
+  // Parse results into a cards block
+  columns: (results) => {
+    const blockContents = [];
+    results.forEach((result) => {
+      const row = [];
+      const divLeft = div();
+      const columnImage = createOptimizedPicture(result.image);
+      divLeft.appendChild(columnImage);
+      const divRight = div();
+      const dateDiv = div({ class: 'date' }, result.start);
+      const divTitle = div({ class: 'title' }, h3(result.title));
+      const divDescription = div({ class: 'description' }, result.eventdescription);
+      const divPath = div({ class: 'path' }, result.path);
+      divRight.appendChild(dateDiv);
+      divRight.appendChild(divTitle);
+      divRight.appendChild(divDescription);
+      divRight.appendChild(divPath);
+      const columnBody = div();
+      columnBody.appendChild(divLeft);
+      columnBody.appendChild(divRight);
+      row.push(columnBody);
+      blockContents.push(row);
+    });
+    return blockContents;
+  },
+};
+
 export async function fetchPlaceholders() {
   window.placeholders = window.placeholders || {};
   const TRANSLATION_KEY_EVENTS = 'events';
@@ -28,5 +63,10 @@ export async function fetchPlaceholders() {
 
 export default async function decorate(block) {
   const placeholders = await fetchPlaceholders();
-  console.log(placeholders[0].data);
+  const yesArray = placeholders[0].data.filter((item) => item.featured === 'yes');
+  const blockContents = resultParsers.columns(yesArray);
+  const builtBlock = buildBlock('columns', blockContents);
+  block.appendChild(builtBlock);
+  decorateBlock(builtBlock);
+  await loadBlock(builtBlock);
 }
