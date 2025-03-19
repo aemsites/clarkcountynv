35b2a39de6952b10b1643f0beba2f693a203a0d9
akasjain <akasjain@adobe.com>
2025-03-19 19:38:34 +0530

    Add title to map-embed

--- blocks/map-embed/map-embed.js
+++ blocks/map-embed/map-embed.js
@@ -1,20 +1,24 @@
-import { iframe } from '../../scripts/dom-helpers.js';
+import {iframe, p} from '../../scripts/dom-helpers.js';
 
 let $mapEmbedUrl = '';
 
 export default function decorate(block) {
   [...block.children].forEach((row) => {
-    if (row.querySelector('a')) {
-      $mapEmbedUrl = row.querySelector('a').href;
+    const mapEmbedEl = row.querySelector('a');
+    if (mapEmbedEl) {
+      const mapLink = mapEmbedEl.href;
       const $iframe = iframe(
         {
-          src: $mapEmbedUrl,
+          src: mapLink,
           allowFullscreen: true,
           frameBorder: 0,
           class: 'map-embed',
         },
       );
       row.replaceWith($iframe);
+      if(mapEmbedEl.textContent.trim() != mapLink) {
+        $iframe.parentElement.appendChild((p({class: 'map-title'}, mapEmbedEl.textContent.trim())));
+      }
     }
   });
 }
