a9e12262b94f5a5ac066af415f410968d2f59cf2
akasjain <akasjain@adobe.com>
2025-04-24 00:29:28 +0530

    Tooltip support

--- blocks/form/util.js
+++ blocks/form/util.js
@@ -1,5 +1,6 @@
 // create a string containing head tags from h1 to h5
 import { defaultErrorMessages } from './constant.js';
+import { span } from '../../scripts/dom-helpers.js';
 
 const headings = Array.from({ length: 5 }, (_, i) => `<h${i + 1}>`).join('');
 const allowedTags = `${headings}<a><b><p><i><em><strong><ul><li><ol>`;
@@ -76,11 +77,10 @@ export function createLabel(fd, tagName = 'label') {
     }
     if (fd.tooltip) {
       label.title = stripTags(fd.tooltip, '');
-      label.classList.add('has-visible-tooltip');
-      
-      const tooltipContent = document.createElement('span');
-      tooltipContent.className = 'tooltip-content';
-      tooltipContent.textContent = stripTags(fd.tooltip, '');
+    }
+
+    if (fd?.properties?.tooltipText) {
+      const tooltipContent = span({ class: 'tooltip-content', 'data-tooltip-text': stripTags(fd.properties.tooltipText, '') }, '?');
       label.appendChild(tooltipContent);
     }
     return label;
