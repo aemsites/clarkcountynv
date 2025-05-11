7d474030d6b5b3e119674ea56cbb27a994677a82
Meet Kumar Jain <meejain@Meets-MacBook-Pro-2.local>
2025-05-12 05:19:53 +0530

    Taking care of Search functionality for the section pages on the home page

--- blocks/search-results/search-results.js
+++ blocks/search-results/search-results.js
@@ -7,7 +7,7 @@ export default async function decorate(block) {
   const gseDivHeader = div({ class: 'gcse-searchresults-only' });
   const gseDiv = div({ class: 'gcse-search' });
   gseDivHeader.setAttribute('data-linkTarget', '_blank');
-  gseDivHeader.setAttribute('data-linkTarget', '_blank');
+  gseDiv.setAttribute('data-linkTarget', '_blank');
   block.innerHTML = '';
   const searchPath = '/search-header';
   if (window.location.pathname.includes(searchPath)) {
