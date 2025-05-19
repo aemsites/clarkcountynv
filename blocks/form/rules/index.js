fa2eb53eed4cb5540d490efdd13b72356b6f6e34
Meet Kumar Jain <meejain@Meets-MacBook-Pro-2.local>
2025-05-20 05:05:04 +0530

    Adding logic for spinner

--- blocks/form/rules/index.js
+++ blocks/form/rules/index.js
@@ -286,8 +286,8 @@ function applyRuleEngine(htmlForm, form, captcha) {
       if (element) {
         element.dispatch({ type: 'click' });
         if (element?.properties?.['fd:buttonType'] === 'submit') {
-          console.log(document.querySelector('.spinner'));
           document.querySelector('.spinner').style.display = 'block';
+          console.log(document.querySelector('.spinner'));
           document.querySelector('main .form form').style.display = 'none';
         }
       }
