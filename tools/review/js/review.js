7eeb0b4d2e83906fc632dd09fd43ad82fa9497d3
smruti.mohapatra <smruti.mohapatra@greatdentalplans.com>
2025-05-21 15:46:28 -0500

    review workflow

--- tools/review/js/review.js
+++ tools/review/js/review.js
@@ -1,3 +1,40 @@
 window.onload = function () {
   document.getElementById('pageurl').innerHTML = window.location.href;
 };
+
+const form = document.querySelector("#reviewform");
+async function sendData() {
+
+  const formData = new FormData(form);
+
+  console.log(formData.get("changeType"));
+  console.log(formData.get("message"));
+  console.log(formData.get("priority"));
+
+console.log("inside send data");
+const data = JSON.stringify({
+    "pageUrl": document.getElementById('pageurl').innerHTML,
+    "requestType": formData.get("changeType"),
+    "criticality": formData.get("priority"),
+    "comments": formData.get("message")
+});
+console.log("data",data);
+
+  try {
+    var req = new XMLHttpRequest();
+  var url = "https://prod-43.usgovtexas.logic.azure.us/workflows/6414b37738a64ad1865219fdcf6bce9c/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=wuv2It6barW-w9zvAuFejn81kppwFGw0tBLqGnV0BSk";
+  req.open("POST", url, true);
+  req.setRequestHeader('Content-Type', 'application/json');
+  req.send(data);
+ 
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+
+// Take over form submission
+form.addEventListener("submit", (event) => {
+  console.log("submit");
+ sendData();
+});
