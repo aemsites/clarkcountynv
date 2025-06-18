6c2853909c9be8cdfaac0d26a9d1de3d8fb58798
Aaron Stewart <90333222+aaronstewart-dev@users.noreply.github.com>
2025-06-18 12:52:21 -0700

    Feature: Issue #1375 - Page Alert Banner (#1385)
    * Feature: Issue #1375 - Added Page Alert banner component.
    
    * Feature: Issue #1375 - Updated Page Alert banner component.

--- blocks/alert-popup/alert-popup.js
+++ blocks/alert-popup/alert-popup.js
@@ -1,11 +1,28 @@
 import { createModal } from '../modal/modal.js';
 
 export default async function decorate(block) {
-  const childNodes = block.children[0]?.childNodes;
-  if (childNodes.length) {
-    const filteredNodes = Array.from(childNodes).filter((node) => node.nodeName === 'DIV');
-    const { showModal, block: modal } = await createModal(filteredNodes);
-    modal.className += ' alert-popup popup';
-    showModal();
+  const type = block.classList.contains('popup') ? 'popup' : 'banner';
+  if (type === 'popup') {
+    const childNodes = block.children[0]?.childNodes;
+    if (childNodes.length) {
+      const filteredNodes = Array.from(childNodes).filter((node) => node.nodeName === 'DIV');
+      if (filteredNodes.length) {
+        const { showModal, block: modal } = await createModal(filteredNodes);
+        modal.className += ' alert-popup popup';
+        const modalContent = modal.querySelector('.modal-content');
+        const icon = modalContent?.querySelector('span.icon');
+        if (icon) {
+          icon.setAttribute('role', 'img');
+          icon.setAttribute('aria-label', `Alert ${type} icon`);
+        }
+        showModal();
+      }
+    }
+  } else {
+    const spanIcon = block.querySelector('span.icon');
+    if (spanIcon) {
+      spanIcon.setAttribute('role', 'img');
+      spanIcon.setAttribute('aria-label', `Alert ${type} icon`);
+    }
   }
 }
