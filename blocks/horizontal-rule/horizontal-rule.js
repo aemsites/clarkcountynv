cecd214f46c6f62c6c5577d96bd0f9a682552538
Aaron Stewart <aaron.stewart@slalom.com>
2025-07-01 10:06:28 -0700

    Fixing linter issues.

--- blocks/horizontal-rule/horizontal-rule.js
+++ blocks/horizontal-rule/horizontal-rule.js
@@ -1,6 +1,6 @@
 import { hr } from '../../scripts/dom-helpers.js';
 
 export default function decorate(block) {
-    const separator = hr({ class: 'separator', 'aria-label': 'Section separator' });
-    block.append(separator);
+  const separator = hr({ class: 'separator', 'aria-label': 'Section separator' });
+  block.append(separator);
 }
