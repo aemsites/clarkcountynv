47833bf816682f20436c0f5b82338890b7b7532e
Aaron Stewart <aaron.stewart@slalom.com>
2025-07-09 17:56:22 -0700

    Refactoring top topics.

--- blocks/top-topics/top-topics.js
+++ blocks/top-topics/top-topics.js
@@ -1,118 +1,114 @@
 // eslint-disable-next-line import/no-unresolved
 import { toClassName } from '../../scripts/aem.js';
 import { getViewPort } from '../../scripts/utils.js';
-import {
-  a,
-} from '../../scripts/dom-helpers.js';
+import { button, div } from '../../scripts/dom-helpers.js';
 
-function hasWrapper(el) {
-  return !!el.firstElementChild && window.getComputedStyle(el.firstElementChild).display === 'block';
-}
+const handleToggle = (event) => {
+  const currentTabBtn = event.target;
+  if (currentTabBtn.getAttribute('aria-selected') === 'true') {
+    return;
+  }
+  const isDesktopViewport = getViewPort() === 'desktop';
+  const block = currentTabBtn.closest('.block');
+  const layout = isDesktopViewport ? block?.querySelector('.top-topics-desktop') : block?.querySelector('.top-topics-mobile');
+  
+  // reset old 
+  const prevTabBtn = layout.querySelector('.tabs-tab[aria-selected="true"]');
+  prevTabBtn?.setAttribute('aria-selected', 'false');
+  const prevTabPanel = layout.querySelector('.tabs-panel[aria-hidden="false"]');
+  prevTabPanel?.setAttribute('aria-hidden', 'true');
+
+  // show new
+  if (currentTabBtn.getAttribute('aria-selected') === 'false') {
+    currentTabBtn.setAttribute('aria-selected', 'true');
+    const currentTabBtnControlsId = currentTabBtn.getAttribute('aria-controls');
+    if (currentTabBtnControlsId) {
+      const currentTabPanel = layout.querySelector(`.tabs-panel[id="${currentTabBtnControlsId}"]`);
+      currentTabPanel?.setAttribute('aria-hidden', 'false');
+    }
+  }
+};
 
 export default async function decorate(block) {
-  // build tablist
-  const tablist = document.createElement('div');
-  tablist.className = 'tabs-list';
-  tablist.setAttribute('role', 'tablist');
 
-  // decorate tabs and tabpanels
-  const tabs = [...block.children].map((child) => child.firstElementChild);
-  tabs.forEach((tab, i) => {
+  const isMobileViewport = getViewPort() === 'mobile';
+  const desktopLayout = div({ class: 'top-topics-desktop' });
+  const desktopSideNav = div({ class: 'sidenav' });
+  const desktopMainContent = div({ class: 'main-content' });
+  const desktopContentFrag = document.createDocumentFragment();
+  const mobileContentFrag = document.createDocumentFragment();
+  const contentFragmentWrapper = document.createDocumentFragment();
+  const mobileLayout = div({ class: 'top-topics-mobile' });
+
+  // build desktop/mobile tablist
+  const desktopTabList = div({ class: 'tabs-list', role: 'tablist' });
+  const mobileTabList = div({ class: 'tabs-list', role: 'tablist' });
+
+  [...block.children].forEach((row, rowIndex) => {
+
+    // tab
+    const tab = row.firstElementChild;
     const id = toClassName(tab.textContent);
     const isNotSearchTab = id.indexOf('try-searching') === -1;
-
-    // decorate tabpanel
-    const tabpanel = block.children[i];
-    if (isNotSearchTab) {
-      tabpanel.className = 'tabs-panel';
-      tabpanel.id = `tabpanel-${id}`;
-      tabpanel.setAttribute('aria-hidden', !!i);
-      tabpanel.setAttribute('aria-labelledby', `tab-${id}`);
-      tabpanel.setAttribute('role', 'tabpanel');
-      if (!hasWrapper(tabpanel.lastElementChild)) {
-        tabpanel.lastElementChild.innerHTML = `<p>${tabpanel.lastElementChild.innerHTML}</p>`;
+    const tabBtn = button(
+      { 
+        class: isNotSearchTab ? 'tabs-tab' : 'search-btn',
+        type: 'button',
+        role: isNotSearchTab ? 'tab' : 'none',
+        ...(isNotSearchTab && { id: `tab-${id}` }),
+        ...(isNotSearchTab && { 'aria-selected': (isMobileViewport ? false : (rowIndex === 0))}),
+        ...(isNotSearchTab && { 'aria-controls': `tabpanel-${id}` }),
       }
-    } else {
-      tabpanel.className = 'tabs-panel';
-      tabpanel.setAttribute('role', 'none');
-    }
+    );
 
-    // build tab button
-    let button = null;
-    //const button = document.createElement('button');
-    if (isNotSearchTab) {
-      button = document.createElement('button');
-      button.className = 'tabs-tab';
-      button.id = `tab-${id}`;
-      button.setAttribute('aria-controls', `tabpanel-${id}`);
-      button.setAttribute('aria-selected', !i);
-      button.setAttribute('role', 'tab');
-      button.setAttribute('type', 'button');
-    } else {
-      button = document.createElement('div');
-      button.classList.add('tab-search');
-      button.setAttribute('role', 'none');
+    const searchBtn = tab.querySelector('a');
+    if (searchBtn) {
+      searchBtn.classList.add('button', 'secondary');
+      const img = searchBtn.querySelector('img');
+      if (img && !img.alt) {
+        img.alt = 'Top topics search icon';
+      }
     }
-    button.innerHTML = tab.innerHTML;
-    if (isNotSearchTab) {
-      button.addEventListener('mouseover', () => {
-        block.querySelectorAll('[role=tabpanel]').forEach((panel) => {
-          panel.setAttribute('aria-hidden', true);
-        });
-        tablist.querySelectorAll('button').forEach((btn) => {
-          if (btn.hasAttribute('role') && btn.getAttribute('role') === 'tab') {
-            btn.setAttribute('aria-selected', false);
-          }
-        });
-        tabpanel.setAttribute('aria-hidden', false);
-        if (button.hasAttribute('role') && button.getAttribute('role') !== 'none') {
-          button.setAttribute('aria-selected', true);
-        }
-        const viewPort = getViewPort();
-        if (viewPort === 'mobile') {
-          button.parentNode.insertBefore(tabpanel, button.nextSibling);
-          const tabLength = (tabpanel.querySelectorAll('li').length);
-          const height = document.querySelector(':root');
-          if (tabLength < 3) {
-            height.style.setProperty('--tab-height', '150px');
-          } else if (tabLength < 5) {
-            height.style.setProperty('--tab-height', '200px');
-          } else if (tabLength < 8) {
-            height.style.setProperty('--tab-height', '300px');
-          } else if (tabLength > 7) {
-            height.style.setProperty('--tab-height', '450px');
-          }
-          button.addEventListener('click', () => {
-            button.parentNode.insertBefore(tabpanel, button.nextSibling);
-          });
-        } else {
-          block.append(tabpanel);
-        }
-      });
-    }
-    tablist.append(button);
-    tab.remove();
+    
+    tabBtn.innerHTML = tab.innerHTML;
+    desktopTabList.append(tabBtn);
+    mobileTabList.append(tabBtn.cloneNode(true));
+
+    // tabpanel
+    const content = row.children.length > 1 ? row.children[1] : [];
+    const tabPanel = div(
+      {
+        class: 'tabs-panel',
+        id: `tabpanel-${id}`,
+        'aria-hidden': (isMobileViewport ? true : (rowIndex !== 0)),
+        'aria-labelledby': `tab-${id}`,
+      }
+    );
+
+    tabPanel.append(content);
+    desktopContentFrag.append(tabPanel);
+    mobileContentFrag.append(tabPanel.cloneNode(true));
+    mobileTabList.append(mobileContentFrag);
+
   });
 
-  block.prepend(tablist);
+  // Desktop 
+  desktopSideNav.append(desktopTabList);
+  desktopMainContent.append(desktopContentFrag);
+  desktopLayout.append(desktopSideNav, desktopMainContent);
 
-  const search = block.querySelector('.tab-search');
-  if (search) {
-    const anchor = search.querySelector('a');
-    anchor?.classList.add('button', 'secondary');
-    const img = search.querySelector('img');
-    if (img && !img.alt) {
-      img.alt = 'Top topics search icon';
-    }
-  }
+  // Mobile
+  mobileLayout.append(mobileTabList);
+  contentFragmentWrapper.append(desktopLayout, mobileLayout);
 
-  function resizeAction() {
-    const viewPort = getViewPort();
-    if (viewPort === 'desktop') {
-      block.querySelectorAll('[role=tabpanel]').forEach((tabpanel) => {
-        block.append(tabpanel);
-      });
-    }
-  }
-  window.addEventListener('resize', resizeAction);
+  block.innerHTML = '';
+  block.append(contentFragmentWrapper);
+
+  // Event listeners
+  const desktopTabs = desktopLayout.querySelectorAll('.tabs-tab');
+  const mobileTabs = mobileLayout.querySelectorAll('.tabs-tab');
+  desktopTabs?.forEach((btn) => btn.addEventListener('mouseover', handleToggle));
+  mobileTabs?.forEach((btn) => btn.addEventListener('click', handleToggle));
+
+  // TODO: Add resize event
 }
