e03d854892ee62d0caa7c149857ac2333f2ce29f
smruti.mohapatra <smruti.mohapatra@greatdentalplans.com>
2025-07-28 09:17:52 -0500

    added bannerUrl to news

---
+++ blocks/news-banner/news-banner.js
@@ -0,0 +1,21 @@
+export default function decorate(block) {
+  const placeholder = block.querySelector('picture');
+    if(placeholder){
+      const image = placeholder.querySelectorAll("source")[0].srcset;
+      setMetaTag("bannerurl",image);
+    }
+    const currentUL = block.querySelector('div');
+    currentUL.parentElement.parentElement.remove();
+}
+
+function setMetaTag(name, content) {
+  let meta = document.querySelector(`meta[name="${name}"]`);
+  if (meta) {
+    meta.setAttribute('content', content);
+  } else {
+    meta = document.createElement('meta');
+    meta.setAttribute('name', name);
+    meta.setAttribute('content', content);
+    document.head.appendChild(meta);
+  }
+}
