1bda8eeed68dd59d61291b97e040b344bc37fbda
smruti.mohapatra <smruti.mohapatra@greatdentalplans.com>
2025-08-05 09:34:12 -0500

    hardcoded asset revert main

--- blocks/newslist/newslist.js
+++ blocks/newslist/newslist.js
@@ -5,8 +5,11 @@ import {
   h3,
 } from '../../scripts/dom-helpers.js';
 import {
-  buildBlock, decorateBlock, loadBlock, createOptimizedPicture,
+  buildBlock, decorateBlock, loadBlock, createOptimizedPicture, fetchPlaceholders
 } from '../../scripts/aem.js';
+
+const placeholders = await fetchPlaceholders();
+const CLARKLOGO = placeholders.clarkcountylogo;
 
 class News {
   constructor(newsTitle, newsDescription, newsPath, newsPublished, newsImage, newsCategory) {
@@ -40,7 +43,7 @@ const resultParsers = {
         const cardImage = createOptimizedPicture(result.newsImage);
         cardleft.append(cardImage);
       } else {
-        const cardImage = createOptimizedPicture('/news/media_1cd00e6d663e3a8f17a6a71845a2d09cc41f55b6d.png');
+        const cardImage = createOptimizedPicture(CLARKLOGO);
         cardleft.append(cardImage);
       }
       const cardright = div({ class: 'card-right' });
