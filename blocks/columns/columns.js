0283a50dedbcd630670909dde8e4b8cd3dfd01da
Aaron Stewart <aaron.stewart@slalom.com>
2025-08-08 15:39:44 -0700

    Updated news listing component.

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -91,7 +91,6 @@ export default function decorate(block) {
   for (let counter = 0; counter < cols.length; counter += 1) {
     cols[counter].classList.add(`column${counter + 1}`);
     [...cols[counter].children].forEach((child) => {
-      console.log(child);
       child.classList.remove('button-container');
       child.classList.add('columns-paragraph');
       child.classList.add(`column${counter + 1}-paragraph`);
