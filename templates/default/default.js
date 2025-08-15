69c4d8689f99cbebb1d4d0558545f1350997a85d
Aaron Stewart <aaron.stewart@slalom.com>
2025-08-15 10:05:09 -0700

    top topics and left nav mobile on click behaviors fix.

--- templates/default/default.js
+++ templates/default/default.js
@@ -110,6 +110,10 @@ export default async function decorate(doc) {
     const sectionMenuBtn = button({ class: 'section-menu-btn', type: 'button' });
     sectionMenuBtn.innerHTML = $clickElement.innerHTML;
     $clickElement.replaceWith(sectionMenuBtn);
+    const sectionMenuBtnParent = sectionMenuBtn.parentElement;
+    if (sectionMenuBtn.parentElement.classList.contains('default-content-wrapper')) {
+      sectionMenuBtn.parentElement.classList.add('left-nav-default-content-wrapper');
+    }
 
     sectionMenuBtn.addEventListener('click', () => {
       sectionMenuBtn.classList.toggle('active');
