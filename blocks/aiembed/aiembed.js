f5b8e05027d643d9c1796f06e6fc30dd4c9cb020
Jason Lui <Jason.Lui@ClarkCountyNV.gov>
2025-11-24 16:32:42 -0800

    add aiembed block (#1604)
    * add aiembed block
    
    * fix lint errors
    
    * fix css lint
    
    * add aiembed class

---
+++ blocks/aiembed/aiembed.js
@@ -0,0 +1,11 @@
+export default async function decorate(block) {
+  console.log('aiembed block has been accessed');
+
+  block.innerHTML = `<iframe
+    id="ccFrame"
+    src="https://customer.1mind.cloud/cc/embed-v7.html"
+    allow="microphone https://customer.1mind.cloud; camera https://customer.1mind.cloud"
+    sandbox="allow-scripts allow-forms allow-same-origin"
+    style="width:100%; height:100vh; border:0;">
+</iframe>`;
+}
