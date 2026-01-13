8b4bbf727ae1931b43dc19cc2617f1e3ea309d70
Jason Lui <Jason.Lui@ClarkCountyNV.gov>
2026-01-13 09:44:07 -0800

    clean up code

--- blocks/alert-popup/alert-popup.js
+++ blocks/alert-popup/alert-popup.js
@@ -1,24 +1,3 @@
-// TODO: Isolate the auto-closing logic to only the full-width popup (It is currently set to close
-//       any type of alert popup... for instance, right now it also includes the modal)
-//       Also remove any un-needed console logs and old code
-
-// INFO: sessionStorage persists only within the same tab. If you make a new tab, that tab has its
-//       own separate sessionStorage and will thus not have its sessionStorage cookie set. This
-//       will result in the fact that if you close the alert and set the cookie, it will not
-//       reappear on subsequent pages within the same tab. However if you open a new tab and go to
-//       the pages, the alerts will show back up, because the new tab does not have the
-//       sessionStorage cookie set yet.
-//
-//       This obviously also applies to completely new browser sessions (closing google chrome
-//       completely and then reopening it)
-//
-//       Also of note is that if you "Duplicate" the tab (by right clicking the tab, and choosing
-//       the Duplicate option), this will copy the sessionStorage of the copied tab over to the new
-//       one. Keep this in mind if any questions get asked about it.
-
-// WARN: Remember to lint the file before pushing!!!
-//       "npx eslint ./blocks/alert-popup/alert-popup.js"
-
 import { createModal } from '../modal/modal.js';
 import { button, img, div } from '../../scripts/dom-helpers.js';
 
@@ -31,7 +10,6 @@ const handleClose = (e) => {
 
   // set sessionStorage cookie when user closes a full-width popup
   sessionStorage.setItem('closed', 'true');
-  console.log('full-width popup has been closed + sessionStorage set');
 
   setTimeout(() => {
     // get next popup and focus close button
@@ -44,11 +22,10 @@ const handleClose = (e) => {
 
 export default async function decorate(block) {
   if (sessionStorage.getItem('closed') === 'true') {
-    console.log('sessionStorage cookie is set to true');
+    // if alert-popup (full width) has been closed, do not render it again
     block.innerHTML = '';
   } else {
-    console.log('sessionStorage cookie not set...');
-
+    // otherwise, render it normally.
     const type = block.classList.contains('popup') ? 'popup' : 'banner';
     if (type === 'popup') {
       if (!block.classList.contains('full-width')) {
