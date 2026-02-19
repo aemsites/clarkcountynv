a7fd04481b1e1a631489ef6f35868ce29908da94
Jason Lui <Jason.Lui@ClarkCountyNV.gov>
2026-02-19 07:02:26 -0800

    update aiembed block from v7 to v8

--- blocks/aiembed/aiembed.js
+++ blocks/aiembed/aiembed.js
@@ -3,7 +3,7 @@ export default async function decorate(block) {
 
   block.innerHTML = `<iframe
     id="ccFrame"
-    src="https://customer.1mind.cloud/cc/embed-v7.html"
+    src="https://customer.1mind.cloud/cc/embed-v8.html"
     allow="microphone https://customer.1mind.cloud; camera https://customer.1mind.cloud"
     sandbox="allow-scripts allow-forms allow-same-origin"
     style="width:100%; height:100vh; border:0;">
