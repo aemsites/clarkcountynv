39aeed8a49b7a04fdf332232bcc2193b9b49c88b
Jason Lui <Jason.Lui@ClarkCountyNV.gov>
2026-02-24 16:10:11 -0800

    make iframe container larger, consolidate styling to .css

--- blocks/aiembed/aiembed.js
+++ blocks/aiembed/aiembed.js
@@ -1,9 +1,9 @@
 export default async function decorate(block) {
   block.innerHTML = `<iframe
+    class="oneMindEmbed"
     id="ccFrame"
     src="https://customer.1mind.cloud/cc/embed-v8.html"
     allow="microphone https://customer.1mind.cloud; camera https://customer.1mind.cloud"
-    sandbox="allow-scripts allow-forms allow-same-origin"
-    style="width:100%; height:100vh; border:0;">
+    sandbox="allow-scripts allow-forms allow-same-origin">
 </iframe>`;
 }
