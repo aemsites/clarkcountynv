fc5128ce9d9bbf61e51480d50ba5d712b11f83b1
Jason Lui <Jason.Lui@ClarkCountyNV.gov>
2026-02-24 16:15:27 -0800

    remove unused class

--- blocks/aiembed/aiembed.js
+++ blocks/aiembed/aiembed.js
@@ -1,6 +1,5 @@
 export default async function decorate(block) {
   block.innerHTML = `<iframe
-    class="oneMindEmbed"
     id="ccFrame"
     src="https://customer.1mind.cloud/cc/embed-v8.html"
     allow="microphone https://customer.1mind.cloud; camera https://customer.1mind.cloud"
