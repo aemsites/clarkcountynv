b889b767a1cd6392b71fbda51a681d9024cb68eb
Jason Lui <Jason.Lui@ClarkCountyNV.gov>
2026-04-21 11:44:12 -0700

    remove old marketing agency analytics tag (#1639)

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -61,18 +61,10 @@ if (window.location.pathname.includes('/calendar')) {
 /* Google Tag Manager & Google Analytics */
 
 async function loadAnalyticsScripts() {
-  const uniquePaths = '/foster_care/';
-  if (window.location.pathname.includes(uniquePaths)) {
-    // google tag manager
-    const gtmId = 'GTM-WGNDK2R';
-    // eslint-disable-next-line
+  // google tag manager
+  const gtmId = 'GTM-5NWDH65B';
+  // eslint-disable-next-line
     (function (w, d, s, l, i) { w[l] = w[l] || []; w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' }); var f = d.getElementsByTagName(s)[0], j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f); })(window, document, 'script', 'dataLayer', gtmId);
-  } else {
-    // google tag manager
-    const gtmId = 'GTM-5NWDH65B';
-    // eslint-disable-next-line
-    (function (w, d, s, l, i) { w[l] = w[l] || []; w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' }); var f = d.getElementsByTagName(s)[0], j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f); })(window, document, 'script', 'dataLayer', gtmId);
-  }
 }
 
 async function loadPodcastStreamingAudio() {
