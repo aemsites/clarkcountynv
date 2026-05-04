1a7e63bc5049d0291d15057a75f701f111fde4af
Jason Lui <Jason.Lui@ClarkCountyNV.gov>
2026-05-04 10:15:05 -0700

    change to v9-prod chatbot, styling changes to avoid extra overhang

--- blocks/aiembed/aiembed.js
+++ blocks/aiembed/aiembed.js
@@ -1,7 +1,7 @@
 export default async function decorate(block) {
   block.innerHTML = `<iframe
     id="ccFrame"
-    src="https://customer.1mind.cloud/cc/embed-v8.html"
+    src="https://customer.1mind.cloud/cc/embed-v9-prod.html"
     allow="microphone https://customer.1mind.cloud; camera https://customer.1mind.cloud"
     sandbox="allow-scripts allow-forms allow-same-origin">
 </iframe>`;
