5cad469a8da0191df371e6ca84ec5e831bdf14f7
Jason Lui <Jason.Lui@ClarkCountyNV.gov>
2026-05-13 10:59:44 -0700

    add "allow-popups" attribute to sandbox (#1645)

--- blocks/aiembed/aiembed.js
+++ blocks/aiembed/aiembed.js
@@ -3,6 +3,6 @@ export default async function decorate(block) {
     id="ccFrame"
     src="https://customer.1mind.cloud/cc/embed-v9-prod.html"
     allow="microphone https://customer.1mind.cloud; camera https://customer.1mind.cloud"
-    sandbox="allow-scripts allow-forms allow-same-origin">
+    sandbox="allow-scripts allow-forms allow-same-origin allow-popups">
 </iframe>`;
 }
